ad209c731e47fc7fa96cc90657caa31898e042c7
Igor <test@mail.ru>
2023-09-10 21:10:40 +0300

    2+

--- task24/Task_24.py
+++ task24/Task_24.py
@@ -10,15 +10,15 @@
 
 n = int(input("Введите количество кустов: "))
 while n < 3:
-    dictionary = input("Вы ошиблись! Количество кустов не должно быть меньше 3-х! \nВведите количество кустов: ")
+    dictionary = input("Ошибка! Количество кустов должно быть больше 3-х! \nВведите количество кустов: ")
 
 numbers = list()
 for i in range(n):
-    numbers.append(int(input(f"Введите количество ягод на кусту {i + 1}: ")))
+    numbers.append(int(input(f"Введите количество ягод на кусте {i + 1}: ")))
 
 numbers_count = list()
 for i in range(len(numbers) - 1):
     numbers_count.append(numbers[i - 1] + numbers[i] + numbers[i + 1])
 numbers_count.append(numbers[-2] + numbers[-1] + numbers[0])
 
-print(f"Максимального числа ягод, которое может собрать за один заход собирающий модуль: {max(numbers_count)}")
+print(f"Модуль может собрать за один заход такое максимальное количество ягод: {max(numbers_count)}")
